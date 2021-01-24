f0cb638c2a287bc5f818cf2aaa08e42330311798
lynnfgsga <50407939+lynnfgsga@users.noreply.github.com>
2021-01-24 15:59:41 +0800

    Add files via upload

--- linj.js
+++ linj.js
@@ -12,31 +12,3 @@ xiaokang.imgList = [
   ];
   // 调用随机背景
   xiaokang.randomBg();
-
-xiaokang.footFish();
-$(document).ready(function(e){
-    $('.copyright').html('©2020 <i class="fa-fw fas fa-heart card-announcement-animation cc_pointer"></i> Hexo');
-})
-
-$(document).ready(function(e){
-    $('.framework-info').html('本站已运行<SPAN id=span_dt_dt style="color: #fff;"></SPAN>');
-})
-
-function show_date_time(){
-window.setTimeout("show_date_time()", 1000);
-BirthDay=new Date("11/16/2020 0:0:0");
-today=new Date();
-timeold=(today.getTime()-BirthDay.getTime());
-sectimeold=timeold/1000
-secondsold=Math.floor(sectimeold);
-msPerDay=24*60*60*1000
-e_daysold=timeold/msPerDay
-daysold=Math.floor(e_daysold);
-e_hrsold=(e_daysold-daysold)*24;
-hrsold=Math.floor(e_hrsold);
-e_minsold=(e_hrsold-hrsold)*60;
-minsold=Math.floor((e_hrsold-hrsold)*60);
-seconds=Math.floor((e_minsold-minsold)*60);
-span_dt_dt.innerHTML=' <font style=color:#00bcd4>'+daysold+'</font> 天 <font style=color:#b2ebf2>'+hrsold+'</font> 时 <font style=color:#ff5722>'+minsold+'</font> 分 <font style=color:#dd2c00>'+seconds+'</font> 秒';
-}
-show_date_time();
