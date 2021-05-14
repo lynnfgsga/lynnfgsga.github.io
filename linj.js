58519a739706ff9c0677c64e4aa17f3228489554
lynnfgsga <50407939+lynnfgsga@users.noreply.github.com>
2021-05-14 20:46:19 +0800

    Add files via upload

--- linj.js
+++ linj.js
@@ -3,12 +3,13 @@ var xiaokang = new xkTool();
 xiaokang.imgList = [
     "https://pic.downk.cc/item/5fdab6363ffa7d37b30d2d5a.png",
     "https://pic.downk.cc/item/5fdab6363ffa7d37b30d2d5d.jpg",
-    "https://pic.downk.cc/item/5fdab63e3ffa7d37b30d3205.jpg",
-    "https://pic.downk.cc/item/5fdab63e3ffa7d37b30d3208.jpg",
+    "https://img.imgdb.cn/item/6088f2f3d1a9ae528f384101.png",
+    "https://pic.imgdb.cn/item/609e0186d1a9ae528ffff80c.jpg",
     "https://pic.downk.cc/item/5fdab63e3ffa7d37b30d320a.png",
     "https://pic.downk.cc/item/5fdab63e3ffa7d37b30d320e.png",
-    "https://img.imgdb.cn/item/600becac3ffa7d37b3b55fa6.png",
     "https://pic.downk.cc/item/5fe492c13ffa7d37b30839ec.jpg",
+    "https://pic.imgdb.cn/item/609dfeafd1a9ae528fe3a40a.jpg",
+    "https://pic.imgdb.cn/item/600becac3ffa7d37b3b55fa6.png",
   ];
   // 调用随机背景
   xiaokang.randomBg();
